5fdcf96916cb4b62dce158b3ec4f115be3720c0d
Mike Grabowski <grabbou@gmail.com>
2024-12-07 17:28:26 +0100

    remove one example and small tweaks

--- packages/framework/src/teamwork.ts
+++ packages/framework/src/teamwork.ts
@@ -7,45 +7,49 @@ import { Message } from './types.js'
 import { Workflow } from './workflow.js'
 
 async function execute(workflow: Workflow, messages: Message[]): Promise<string> {
-  // eslint-disable-next-line no-constant-condition
   const task = await getNextTask(workflow.provider, messages)
   if (!task) {
-    return messages.at(-1)!.content as string // end of the recursion
-  }
-
-  if (workflow.maxIterations && messages.length > workflow.maxIterations) {
-    console.debug('Max iterations exceeded ', workflow.maxIterations)
     return messages.at(-1)!.content as string
   }
 
+  // tbd: implement `final answer` flow
+  if (messages.length > workflow.maxIterations) {
+    return messages.at(-1)!.content as string
+  }
+
+  // tbd: get rid of console.logs, use telemetry instead
   console.log('🚀 Next task:', task)
 
-  messages.push({
-    role: 'user',
-    content: task,
-  })
-
-  // tbd: this throws, handle it
   const selectedAgent = await selectAgent(workflow.provider, task, workflow.members)
   console.log('🚀 Selected agent:', selectedAgent.role)
 
-  // tbd: this should just be a try/catch
+  const agentRequest: Message[] = [
+    ...messages,
+    {
+      role: 'user',
+      content: task,
+    },
+  ]
+
   // tbd: do not return string, but more information or keep memory in agent
   try {
-    const result = await executeTaskWithAgent(selectedAgent, messages, workflow.members)
-    messages.push({
-      role: 'assistant',
-      content: result,
-    })
+    const result = await executeTaskWithAgent(selectedAgent, agentRequest, workflow.members)
+    return execute(workflow, [
+      ...agentRequest,
+      {
+        role: 'assistant',
+        content: result,
+      },
+    ])
   } catch (error) {
-    console.log('🚀 Task error:', error)
-    messages.push({
-      role: 'assistant',
-      content: error instanceof Error ? error.message : 'Unknown error',
-    })
+    return execute(workflow, [
+      ...agentRequest,
+      {
+        role: 'assistant',
+        content: error instanceof Error ? error.message : 'Unknown error',
+      },
+    ])
   }
-
-  return execute(workflow, messages)
 }
 
 export async function teamwork(workflow: Workflow): Promise<string> {
