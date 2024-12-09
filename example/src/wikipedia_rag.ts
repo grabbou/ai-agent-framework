3327365bbda2ac01e295e19fc3dfd2f468020d7b
Piotr Karwatka <piotr.karwatka@gmail.com>
2024-12-09 15:39:29 +0100

    feat: ragTool + example

---
+++ example/src/wikipedia_rag.ts
@@ -0,0 +1,52 @@
+import { agent } from '@dead-simple-ai-agent/framework/agent'
+import { teamwork } from '@dead-simple-ai-agent/framework/teamwork'
+import { logger } from '@dead-simple-ai-agent/framework/telemetry'
+import { solution, workflow } from '@dead-simple-ai-agent/framework/workflow'
+
+import { lookupWikipedia } from '../tools.js'
+import { ragSave, ragSearch } from './tools/vector/index.js'
+
+const wikipediaIndexer = agent({
+  role: 'Wikipedia Indexer',
+  description: `
+    You are skilled at reading and understanding the context of Wikipedia articles.
+    You create an index of embeddings for each sentence to make it easy for other team members to search.
+  `,
+  tools: {
+    lookupWikipedia,
+    ragSave,
+  },
+})
+
+const reportCompiler = agent({
+  role: 'Report Compiler',
+  description: `
+    You are skilled at compiling information from various sources into a coherent report.
+    You have access to RAG database with indexed sentences of Wikipedia articles.
+    You are making use of it to find relevant information.
+  `,
+  tools: {
+    ragSearch,
+  },
+})
+
+const wikipediaResearch = workflow({
+  members: [wikipediaIndexer, reportCompiler],
+  description: `
+    Find information about John III Sobieski.
+    Index the data into vector database.
+    List exact full paragraphs related to:
+     - Battle of Vienna.
+     - John III Youth,
+     - John III later years and death.
+  `,
+  output: `
+    Report with:
+     - bullet points - list of 2 sentences per each topic from vector db.
+  `,
+  snapshot: logger,
+})
+
+const result = await teamwork(wikipediaResearch)
+
+console.log(solution(result))
