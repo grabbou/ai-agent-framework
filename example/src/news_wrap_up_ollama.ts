df5675b79cd0da7fb68e37337c1a1f5aa1c273a2
Piotr Karwatka <piotr.karwatka@gmail.com>
2024-12-13 14:27:18 +0100

    feat: news_wrap_up_ollama

--- example/src/news_wrap_up_ollama.ts
+++ example/src/news_wrap_up_ollama.ts
@@ -4,18 +4,26 @@ import { getCurrentDate } from '@fabrice-ai/tools/date'
 import { getApiKey } from '@fabrice-ai/tools/utils'
 import { createWebSearchTools } from '@fabrice-ai/tools/webSearch'
 import { agent } from 'fabrice-ai/agent'
+import { openai } from 'fabrice-ai/models'
 import { solution } from 'fabrice-ai/solution'
 import { teamwork } from 'fabrice-ai/teamwork'
 import { logger } from 'fabrice-ai/telemetry'
 import { workflow } from 'fabrice-ai/workflow'
 
 const apiKey = await getApiKey('Serply.io API', 'SERPLY_API_KEY')
+const ollama = openai({
+  options: {
+    baseURL: 'http://localhost:11434/v1',
+  },
+  model: 'llama3.1',
+})
 
 const { googleSearch } = createWebSearchTools({
   apiKey,
 })
 
 const newsResearcher = agent({
+  provider: ollama,
   description: `
     You are skilled at searching the News over Web.
     Your job is to get the news from the last week.
@@ -27,6 +35,7 @@ const newsResearcher = agent({
 })
 
 const newsReader = agent({
+  provider: ollama,
   description: `
     You're greatly skilled at reading and summarizing news headlines.
     Other team members rely on you to get the gist of the news.
@@ -35,6 +44,7 @@ const newsReader = agent({
 })
 
 const wrapupRedactor = agent({
+  provider: ollama,
   description: `
     Your role is to wrap up the news and trends for the last week into a comprehensive report.
     Generalization is also one of your powerfull skills, however you're not a fortune teller.
