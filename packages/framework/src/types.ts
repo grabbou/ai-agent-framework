4f7c5502e11151fbc4a740f8762e0dc99dfa7b28
Sunil Pai <spai@cloudflare.com>
2024-12-14 11:56:49 +0000

    fix: add missing `RequiredOptionals<T>` type

--- packages/framework/src/types.ts
+++ packages/framework/src/types.ts
@@ -2,3 +2,8 @@ import type { ChatCompletionMessageParam } from 'openai/resources/chat/completio
 
 export type Message = ChatCompletionMessageParam
 export type MessageContent = Message['content']
+
+// like Required<T> but ONLY pick optional fields
+export type RequiredOptionals<T> = {
+  [K in keyof T as undefined extends T[K] ? K : never]-?: T[K]
+}
