74e024a1563734511059a3c1043da8f574c64ebb
Mike Grabowski <grabbou@gmail.com>
2024-12-15 23:52:21 +0400

    chore

--- example/src/medical_survey.ts
+++ example/src/medical_survey.ts
@@ -1,12 +1,8 @@
-import { grok } from 'fabrice-ai/providers/grok'
 import { solution } from 'fabrice-ai/solution'
 import { teamwork } from 'fabrice-ai/teamwork'
 
 import { preVisitNoteWorkflow } from './medical_survey/workflow.js'
 
-const result = await teamwork({
-  ...preVisitNoteWorkflow,
-  provider: grok(),
-})
+const result = await teamwork(preVisitNoteWorkflow)
 
 console.log(solution(result))
